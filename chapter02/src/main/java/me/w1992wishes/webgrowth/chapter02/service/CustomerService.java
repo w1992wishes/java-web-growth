a8b58c211cacf39fb203eb2d40044999561a2a7c
wanqinfeng <571716844@qq.com>
2017-12-18 17:47:29 +0800

    chapter02 为web应用添加简单业务，完善部分
    Signed-off-by: wanqinfeng <571716844@qq.com>

--- chapter02/src/main/java/me/w1992wishes/webgrowth/chapter02/service/CustomerService.java
+++ chapter02/src/main/java/me/w1992wishes/webgrowth/chapter02/service/CustomerService.java
@@ -1,14 +1,23 @@
 package me.w1992wishes.webgrowth.chapter02.service;
 
+import me.w1992wishes.webgrowth.chapter02.helper.DatabaseHelper;
 import me.w1992wishes.webgrowth.chapter02.model.Customer;
+import me.w1992wishes.webgrowth.chapter02.util.PropsUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * Created by w1992wishes on 2017/12/18.
  */
 public class CustomerService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
 
     /**
      * 获取客户列表
@@ -16,7 +25,29 @@ public class CustomerService {
      * @return
      */
     public List<Customer> getCustomerList(String key){
-        return null;
+        List<Customer> customerList = new ArrayList<>();
+        Connection conn = null;
+        try {
+            String sql = "SELECT * FROM customer";
+            conn = DatabaseHelper.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){
+                Customer customer = new Customer();
+                customer.setId(rs.getLong("id"));
+                customer.setName(rs.getString("name"));
+                customer.setContact(rs.getString("contact"));
+                customer.setTelephone(rs.getString("telephone"));
+                customer.setEmail(rs.getString("email"));
+                customer.setRemark(rs.getString("remark"));
+                customerList.add(customer);
+            }
+        }catch (SQLException e) {
+            LOGGER.error("execute sql query failure", e);
+        }finally {
+            DatabaseHelper.closeConnection(conn);
+        }
+        return customerList;
     }
 
     /**
---
+++ chapter02/src/main/java/me/w1992wishes/webgrowth/chapter02/util/StringUtil.java
@@ -0,0 +1,21 @@
+package me.w1992wishes.webgrowth.chapter02.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by w1992wishes on 2017/12/18.
+ */
+public final class StringUtil {
+
+    public static boolean isEmpty(String str){
+        if (str != null){
+            str.trim();
+        }
+        return StringUtils.isEmpty(str);
+    }
+
+    public static boolean isNotEmpty(String str){
+        return !isEmpty(str);
+    }
+
+}
